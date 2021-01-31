35adae30fb7b57cff915397fbaea95b89b7fca9f
Matt Zhang <mcmattzhang@gmail.com>
2021-01-30 22:55:35 -0500

    deleting comments working

--- server/routes/taskActions.js
+++ server/routes/taskActions.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../schema/UserSchema");
 const Task = require("../schema/TaskSchema");
 const Comment = require("../schema/CommentSchema");
+const { ObjectID } = require('mongodb');
 
 router.post('/addComment',passport.authenticate('jwt',{session : false}), async (req,res)=>{
     const {task, comment} = req.body;
@@ -31,6 +32,15 @@ router.get('/getComments/:id', async (req,res)=>{
 router.get("/getTasks", async (req, res)=>{
     const taskList = await Task.find({endTime: {$gte: Date.now()}}).sort({endTime: 1});
     return res.json({tasks: taskList});
+});
+
+router.delete("/deleteComment/:commentId",passport.authenticate('jwt',{session : false}), async (req, res)=>{
+    const targetComment = await Comment.findById(req.params.commentId).catch(err=>{res.status(400).json(err); return})
+    if (!targetComment) return res.status(400).json({message: `Comment with ID ${req.params.commentId} not found!`});
+    if (req.user.username != targetComment.user) return res.status(403).json({message : "Don't try to delete someone else's comment!"});
+    await Comment.deleteOne({ "_id" : ObjectID(targetComment._id) }).catch(err=>{res.status(400).json(err); return});
+    return res.status(200).json({message : `Successfully removed comment with ID ${targetComment._id}`})
+    
 });
 
 module.exports = router;
