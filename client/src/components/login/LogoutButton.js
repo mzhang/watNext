e90f333656af2eaf1398c783f407afd04f5f4491
Matt Zhang <mcmattzhang@gmail.com>
2021-02-15 02:21:52 -0500

    made axios fcalls consistent

--- client/src/components/login/LogoutButton.js
+++ client/src/components/login/LogoutButton.js
@@ -5,11 +5,8 @@ import axios from 'axios';
 export default function LogoutButton() {
 
     const logout = async () => {
-        await axios({
-        method: 'get',
-        url: 'http://localhost:4000/user/logout'
-        })}
-    
+        await axios.get('http://localhost:4000/user/logout')
+    }
     return (
         <Button variant="contained" color="primary" onClick={() => { logout() }}>Logout</Button>
     )
