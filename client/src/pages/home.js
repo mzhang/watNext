79318ffc7e8abe8867f97bacc9c24c3364519b77
Matt Zhang <mcmattzhang@gmail.com>
2021-02-16 16:34:57 -0500

    added quick auth check to home

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+    const [isLoggedIn, setIsLoggedIn] = useState(null);
+    useEffect(()=>{
+        const getLoggedInStatus = async () => {
+            const data = await axios.get("http://localhost:4000/user/authenticated")
+            setIsLoggedIn(data.status != 401) 
+        }
+        getLoggedInStatus();
+    }, [])
     return (
         <div>
             <h1>Home</h1>
+            {isLoggedIn === null? null : <h2>You are {isLoggedIn ? "" : "not"} logged in!</h2>}
+            
         </div>
     )
 }
