4be3e6c38e27642586238b81f0f40a277b612243
Matt Zhang <mcmattzhang@gmail.com>
2021-02-16 21:59:50 -0500

    new toast implementation

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,30 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import AuthContext from './AuthContext'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import axios from 'axios';
 axios.defaults.withCredentials = true;
+axios.interceptors.response.use(response => {
+    return response;
+ }, error => {
+   if (error.response.status === 401) {
+    toast.error("👮 You're unauthorized!", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+   }
+   return error;
+ });
 
-ReactDOM.render(<AuthContext><App /></AuthContext>, document.getElementById('root'));
+ReactDOM.render(
+    <AuthContext>
+        <App />
+        <ToastContainer />
+    </AuthContext>, document.getElementById('root'));
