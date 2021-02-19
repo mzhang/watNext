ec76fe10b2265ee54adc282028c4f2c60f67a628
Matt Zhang <mcmattzhang@gmail.com>
2021-02-18 23:36:09 -0500

    added custom theming

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,28 @@ import Home from './pages/Home';
 import Cards from './pages/Cards'
 import ComponentLibrary from './pages/ComponentLibrary'
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Arial"
+  },
+  palette: {
+    primary: {
+      main: "#8EB19D",
+    }
+  }
+});
+
 function App() { 
 
   return (
-    <>
+    <MuiThemeProvider theme={theme}>
       <Router>
         <Home path="/" />
         <Cards path="/cards" />
         <ComponentLibrary path="/componentLibrary" />
       </Router>
-    </>
+    </MuiThemeProvider>
   )
   
 }
