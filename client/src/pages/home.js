b4855ba87916ff5e9f264c20db8e8bb263fd532d
Matt Zhang <mcmattzhang@gmail.com>
2021-02-26 22:08:07 -0500

    changed home to better suit navbar components

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,13 +3,14 @@ import { AuthContext } from '../AuthContext';
 import LogoutButton from "../components/login/LogoutButton"
 import LoginForm from "../components/login/LoginForm"
 import TaskDeck from '../components/tasks/TaskDeck'
+
 export default function Home() {
     const {user, isLoggedIn} = useContext(AuthContext);
     return (
         <>
-            <h1>Home</h1>
+            {/* <h1>Home</h1>
             {user === null ? null : <h2>{user ? `Hey ${user.username}! `:"" }You are {isLoggedIn ? "" : "not"} logged in!</h2>}
-            <LogoutButton />
+            <LogoutButton /> */}
             <LoginForm />
             <TaskDeck style={{
                 display: "inline-flex",
