6079098cf1d19a03e691cc9c6e4042cc980e556b
Matt Zhang <mcmattzhang@gmail.com>
2021-02-26 22:07:21 -0500

    added new colors, navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,10 @@ import { Router } from '@reach/router';
 import Home from './pages/Home';
 import Cards from './pages/Cards'
 import ComponentLibrary from './pages/ComponentLibrary'
+import Navbar from './components/Navbar'
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 const theme = createMuiTheme({
   typography: {
     fontFamily: "Arial"
@@ -12,6 +14,9 @@ const theme = createMuiTheme({
   palette: {
     primary: {
       main: "#8EB19D",
+    },
+    secondary: {
+      main: "#8CC7A1"
     }
   }
 });
@@ -20,6 +25,8 @@ function App() {
 
   return (
     <MuiThemeProvider theme={theme}>
+
+      <Navbar />
       <Router>
         <Home path="/" />
         <Cards path="/cards" />
