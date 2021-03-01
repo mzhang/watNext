8f8d8839ab3f166b7410ba178554afb521779efb
Matt Zhang <mcmattzhang@gmail.com>
2021-03-01 16:52:40 -0500

    fixed bootstrap -> material ui cards

--- client/src/components/comments/CommentCard.js
+++ client/src/components/comments/CommentCard.js
@@ -1,6 +1,5 @@
 import React from "react"
-import Card from "react-bootstrap/Card"
-// import CardActions from '@material-ui/core/CardActions';
+import Card from "@material-ui/core/Card"
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import commentCardStyle from "./CommentCard.module.css"
