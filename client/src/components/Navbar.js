b4450734a70870074a2ee0c25ac95a967a46f52c
Matt Zhang <mcmattzhang@gmail.com>
2021-02-28 21:14:40 -0500

    used login modal to summon login screen

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,21 +3,21 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import LogoutButton from './login/LogoutButton';
+import LoginModal from './login/LoginModal'
 
 import React, { useContext } from 'react';
 import { AuthContext } from '../AuthContext';
 
 export default function Navbar() {
-    const {user, isLoggedIn} = useContext(AuthContext);
+    const { user, isLoggedIn } = useContext(AuthContext);
 
-    const loginButton = (<Button>Login</Button>)
     return (
     <AppBar position="static">
         <Toolbar>
             <Typography variant="h6" style={{fontWeight: "700", flexGrow:1}}>
             📌 {user?.username ? user.username : "SE25"}'s Tasks
             </Typography>
-            {isLoggedIn ? <LogoutButton /> : loginButton}
+            {isLoggedIn ? <LogoutButton /> : <LoginModal />}
         </Toolbar>
     </AppBar>)
 }
