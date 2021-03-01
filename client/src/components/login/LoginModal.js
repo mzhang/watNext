bd9b2dbf43da701ac659174767f53de2887198d2
Matt Zhang <mcmattzhang@gmail.com>
2021-02-28 21:05:23 -0500

    added summoner for login/register form

---
+++ client/src/components/login/LoginModal.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {Modal, Fade, Backdrop, Container, Button} from '@material-ui/core/';
+// import { makeStyles } from '@material-ui/core/styles';
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
+
+export default function TaskModal(props) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {setOpen(true)};
+  const handleClose = () => {setOpen(false)};
+
+  return (
+    <>
+      
+      <Button onClick={handleOpen} variant="contained" color="secondary">Login</Button>
+      
+      <Modal
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{timeout: 500}}
+      >
+        <Fade in={open}>
+          <Container 
+        style={{display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'}}>
+            <LoginForm />
+            <RegisterForm />
+          </Container>
+        </Fade>
+      </Modal>
+    </>
+  );
+}
