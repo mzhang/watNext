8a9a12c93774c9c49f98dd08004fb40eabd27e85
Matt Zhang <mcmattzhang@gmail.com>
2021-02-28 21:06:05 -0500

    done message only shows on logged in clients

--- client/src/components/tasks/TaskCard.js
+++ client/src/components/tasks/TaskCard.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState,useContext } from "react";
 import {Card, CardContent} from "@material-ui/core/"
 import {Typography} from '@material-ui/core/';
 import TaskModal from "./TaskModal"
 import ToggleCompleteButton from "./ToggleCompleteButton"
 import taskCardStyle from "./TaskCard.module.css"
+import { AuthContext } from "../../AuthContext.js"
 
 export default function TaskCard(props) {
     // const [ opacity, setOpacity ] = useState(props.isDone ? 0.15 : 1);
     const [isDone, setIsDone] = useState(props.isDone);
+    const { user, isLoggedIn } = useContext(AuthContext);
+
 
     // useEffect(() => {
     //     setOpacity(props.isDone ? 0.15 : 1) 
@@ -26,8 +29,8 @@ export default function TaskCard(props) {
                 {props.endTime}
             </Typography>
         </CardContent>
-        <div style={{display: "flex", justifyContent: "center", flexWrap: "wrap"}}>
-            <ToggleCompleteButton id={props.id} isDone={isDone} setIsDone={setIsDone}/>
+        <div style={{display: "flex", justifyContent: "center", flexWrap: "wrap"}}> 
+            { isLoggedIn ? <ToggleCompleteButton id={props.id} isDone={isDone} setIsDone={setIsDone}/> : "" }
             <TaskModal id={props.id} commentCount={props.commentCount}/>  
         </div>
         </Card>
