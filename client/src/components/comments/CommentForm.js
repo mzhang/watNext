0066598b10fd937c7438f38d0e85c3c05b258b77
Matt Zhang <mcmattzhang@gmail.com>
2021-02-28 23:21:24 -0500

    disabled comment button when logged out

--- client/src/components/comments/CommentForm.js
+++ client/src/components/comments/CommentForm.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import axios from 'axios';
 import {Button, TextField, Container} from '@material-ui/core/';
+import { AuthContext } from '../../AuthContext';
 
 export default function RegisterForm(props) { 
   const [comment, setComment] = useState("");
+  const { isLoggedIn } = useContext(AuthContext)
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -16,6 +18,10 @@ export default function RegisterForm(props) {
     setComment("");
     await axios.post('http://localhost:4000/task/addComment', commentPayload).catch(err => console.log(err))
   }
+  
+  const submitButton = <Button variant="contained" color="primary" type="submit">Post Comment</Button>;
+  const disabledButton = <Button variant="contained" disabled>Log in to comment!</Button>;
+  
 
   return (
         <form noValidate onSubmit={handleSubmit} style={{display:"grid", padding: "5%"}}> 
@@ -29,7 +35,7 @@ export default function RegisterForm(props) {
               value={comment}
               onChange={e => setComment(e.target.value)}
             />
-            <Button variant="contained" color="primary" type="submit">Post Comment</Button>
+            {isLoggedIn ? submitButton : disabledButton}
         </form>
   )
   
--- client/src/components/tasks/TaskModal.js
+++ client/src/components/tasks/TaskModal.js
@@ -3,11 +3,9 @@ import {Dialog, Fade, Backdrop, Container, IconButton} from '@material-ui/core/'
 // import { makeStyles } from '@material-ui/core/styles';
 import CommentDeck from '../comments/CommentDeck';
 import CommentForm from '../comments/CommentForm';
-import { AuthContext } from '../../AuthContext'
 
 export default function TaskModal(props) {
   const [open, setOpen] = useState(false);
-  const { isLoggedIn } = useContext(AuthContext)
   
 
   const handleOpen = () => {setOpen(true)};
@@ -20,7 +18,7 @@ export default function TaskModal(props) {
       
       <Dialog open={open} onClose={handleClose} scroll="body">
           <CommentDeck id={props.id} />
-          { isLoggedIn ? <CommentForm id={props.id} /> : "" }
+          <CommentForm id={props.id} />
       </Dialog>
     </>
   );
