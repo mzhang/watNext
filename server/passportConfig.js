dd21693327fa54779dc3dc397baff34822bcabba
Raragyay <let987let987@gmail.com>
2021-03-04 21:18:34 -0500

    Reformatted passportConfig.js

--- server/passportConfig.js
+++ server/passportConfig.js
@@ -1,31 +1,32 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const JwtStrategy = require('passport-jwt').Strategy;
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const JwtStrategy = require('passport-jwt').Strategy
 
-const User = require('./schema/UserSchema');
+const User = require('./schema/UserSchema')
 
 const cookieExtractor = req => {
-    let token = null;
-    if (req&&req.cookies) token = req.cookies["access_token"];
-    return token;
+  let token = null
+  if (req && req.cookies) token = req.cookies['access_token']
+  return token
 }
 
-passport.use(new JwtStrategy({jwtFromRequest: cookieExtractor, secretOrKey : "matt"},
+passport.use(
+  new JwtStrategy({ jwtFromRequest: cookieExtractor, secretOrKey: 'matt' },
     (payload, done) => {
-        User.findById({_id: payload.sub},(err,user)=>{
-            if (err) return done(err,false);
-            if (user) return done(null, user);
-            return done(null,false);
-        })
-}));
+      User.findById({ _id: payload.sub }, (err, user) => {
+        if (err) return done(err, false)
+        if (user) return done(null, user)
+        return done(null, false)
+      })
+    }))
 
 //for logins
-passport.use(new LocalStrategy((username, password, done)=>{
-    User.findOne({username}, (err, user)=>{
-        if (err) return done(err);
-        //if no user found
-        if (!user) return done(null, false);
-        //if password is correct, run done function
-        user.comparePassword(password, done);
-    })
-}));
+passport.use(new LocalStrategy((username, password, done) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) return done(err)
+    //if no user found
+    if (!user) return done(null, false)
+    //if password is correct, run done function
+    user.comparePassword(password, done)
+  })
+}))
