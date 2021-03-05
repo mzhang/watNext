3f0aec9a55f187d710080b31b73f4db60760ff30
Matt Zhang <mcmattzhang@gmail.com>
2021-03-04 23:32:20 -0500

    new color from hash for card border

--- client/src/components/tasks/TaskCard.js
+++ client/src/components/tasks/TaskCard.js
@@ -12,9 +12,25 @@ export default function TaskCard (props) {
 
   const getEmojiFrom = str => str.substring(0, 2)
 
+  function hashCode(str) { // java String#hashCode
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+       hash = str.charCodeAt(i) + ((hash << 3) - hash);
+    }
+    return hash;
+  } 
+
+  function intToRGB(i){
+      var c = (i & 0x00FFFFFF)
+          .toString(16)
+          .toUpperCase();
+
+      return "00000".substring(0, 6 - c.length) + c;
+  }
+
   return (
     <Card className={taskCardStyle.card}
-          style={{ opacity: isDone ? 0.15 : 1 }}>
+          style={{ opacity: isDone ? 0.15 : 1, borderTop: "2px solid #" + intToRGB(hashCode(props.class))}}>
 
       <Typography variant="body2">
         {props.class}
