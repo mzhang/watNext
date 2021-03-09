242d56e36dc3f9a12f139b21ef700d2c028fb876
Matt Zhang <mcmattzhang@gmail.com>
2021-03-08 20:16:39 -0500

    changed font weight

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,23 @@
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import LogoutButton from './login/LogoutButton'
-import LoginModal from './login/LoginModal'
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import LogoutButton from "./login/LogoutButton";
+import LoginModal from "./login/LoginModal";
 
-import { useContext } from 'react'
-import { AuthContext } from '../AuthContext'
+import { useContext } from "react";
+import { AuthContext } from "../AuthContext";
 
-export default function Navbar () {
-  const { user, isLoggedIn } = useContext(AuthContext)
+export default function Navbar() {
+  const { user, isLoggedIn } = useContext(AuthContext);
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6"
-                    style={{ fontWeight: '700', flexGrow: 1 }}>
-          📌 {user?.username ? user.username : 'SE25'}'s Tasks
+        <Typography variant="h6" style={{ fontWeight: "500", flexGrow: 1 }}>
+          📌 {user?.username ? user.username : "SE25"}'s Tasks
         </Typography>
-        {isLoggedIn ? <LogoutButton/> : <LoginModal/>}
+        {isLoggedIn ? <LogoutButton /> : <LoginModal />}
       </Toolbar>
-    </AppBar>)
+    </AppBar>
+  );
 }
